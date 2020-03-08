50861571575ed7ea333889cf2f4b97da9dc4af9f
KingJeremyNg <jeremy97ng@gmail.com>
2020-03-07 19:56:00 -0500

    Finished lab5

---
+++ lab5/lab05.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+pthread_mutex_t count_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t condition_var = PTHREAD_COND_INITIALIZER;
+
+void *functionCount1();
+void *functionCount2();
+int count = 0;
+
+#define COUNT_DONE 9
+
+int main()
+{
+    pthread_t thread1, thread2;
+
+    pthread_create(&thread1, NULL, &functionCount1, NULL);
+    pthread_create(&thread2, NULL, &functionCount2, NULL);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    printf("Final count: %d\n", count);
+
+    exit(0);
+}
+
+// Write numbers 1-3 and 8-10 as permitted by functionCount2()
+void *functionCount1()
+{
+    for (;;)
+    {
+        // Lock mutex and then wait for signal to relase mutex
+        pthread_mutex_lock(&count_mutex);
+
+        // Wait while functionCount2() operates on count.
+        // Mutex is unlocked, waits for condition_var to be signaled
+        pthread_cond_wait(&condition_var, &count_mutex);
+
+        count++;
+        printf("Counter value functionOdd: %d\n", count);
+
+        pthread_mutex_unlock(&count_mutex);
+
+        if (count >= COUNT_DONE)
+        {
+            pthread_cond_signal(&condition_var);
+            return (NULL);
+        }
+    }
+}
+
+// Write numbers 4-7
+void *functionCount2()
+{
+    for (;;)
+    {
+        pthread_mutex_lock(&count_mutex);
+
+        if (count % 2 == 1)
+        {
+            // Condition of if statement has been met.
+            // Signal to free waiting thread by freeing the mutex.
+            // Note: functionCount1() is now permitted to modify "count".
+            pthread_cond_signal(&condition_var);
+        }
+        else
+        {
+            count++;
+            printf("Counter value functionEven: %d\n", count);
+        }
+
+        pthread_mutex_unlock(&count_mutex);
+
+        if (count >= COUNT_DONE)
+        {
+            pthread_cond_signal(&condition_var);
+            return (NULL);
+        }
+    }
+}
